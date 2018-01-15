3bfd39e5ebf3724e8ea616fb3452c9e70ecdb690
Adrien <adrien@arccoza.com>
2018-01-16 02:31:35 +0900

    Add more fns to `ease` and fix `INOUT` typo

--- src/scripts/global.gd
+++ src/scripts/global.gd
@@ -3,40 +3,70 @@ extends Node
 enum {IN, OUT, INOUT, LINEAR, QUAD, CUBIC, QUART, QUINT}
 
 static func ease(from, to, cur, tot, type=[null, LINEAR]):
-	var per = 0.0
+	var per = cur / tot
 	var res = null
 	var change = to - from
 	
 	match type:
-		[IN, CUBIC]:
+		[IN, QUINT]:
 			per = cur / tot
-			res = from + change * per * per * per
+			res = change * per * per * per * per * per
 		[OUT, CUBIC]:
 			per = cur / tot - 1
-			res = from + change * (per * per * per + 1)
-		[OUT, CUBIC]:
+			res = change * (per * per * per * per * per + 1)
+		[INOUT, CUBIC]:
 			per = cur / tot / 2
 			if per < 1:
-				res = from + (change / 2) * per * per * per
+				res = (change / 2) * per * per * per * per * per
 			else:
 				per = per - 2
-				res = from + (change / 2) * (per * per * per + 2)
-		[IN, QUAD]:
+				res = (change / 2) * (per * per * per * per + 2)
+
+		[IN, QUART]:
 			per = cur / tot
-			res = from + change * per * per
-		[OUT, QUAD]:
-			per = cur / tot
-			res = from + change * per * (per - 2)
-		[OUT, QUAD]:
+			res = change * per * per * per * per
+		[OUT, CUBIC]:
+			per = cur / tot - 1
+			res = -change * (per * per * per * per - 1)
+		[INOUT, CUBIC]:
 			per = cur / tot / 2
 			if per < 1:
-				res = from + (change / 2) * per * per
+				res = (change / 2) * per * per * per * per
 			else:
-				res = from + (-change / 2) * ((--per) * (per - 2) - 1)
+				per = per - 2
+				res = (-change / 2) * (per * per * per * per - 2)
+
+		[IN, CUBIC]:
+			per = cur / tot
+			res = change * per * per * per
+		[OUT, CUBIC]:
+			per = cur / tot - 1
+			res = change * (per * per * per + 1)
+		[INOUT, CUBIC]:
+			per = cur / tot / 2
+			if per < 1:
+				res = (change / 2) * per * per * per
+			else:
+				per = per - 2
+				res = (change / 2) * (per * per * per + 2)
+		
+		[IN, QUAD]:
+			per = cur / tot
+			res = change * per * per
+		[OUT, QUAD]:
+			per = cur / tot
+			res = change * per * (per - 2)
+		[INOUT, QUAD]:
+			per = cur / tot / 2
+			if per < 1:
+				res = (change / 2) * per * per
+			else:
+				res = (-change / 2) * ((--per) * (per - 2) - 1)
+		
 		[_, LINEAR]: continue
 		[]: continue
 		[..]:
 			per = cur / tot
-			res = from + change * per
+			res = change * per
 	
-	return res
+	return from + res
