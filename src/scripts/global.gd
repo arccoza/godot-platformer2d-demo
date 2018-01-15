fe4bdf8d88dd580ffea35e1636124b56fb354bdb
Adrien <adrien@arccoza.com>
2018-01-16 04:42:48 +0900

    Fix copy paste errors

--- src/scripts/global.gd
+++ src/scripts/global.gd
@@ -3,17 +3,19 @@ extends Node
 enum {IN, OUT, INOUT, LINEAR, QUAD, CUBIC, QUART, QUINT}
 
 static func ease(from, to, progress, type=[null, LINEAR]):
+	#if progress >= 1.0: return to
 	var p = progress  # 0.0 -> 1.0
 	var step = null
 	var change = to - from
+	print("p: ", p, " change: ", change)
 	
 	match type:
 		[IN, QUINT]:
 			step = change * p * p * p * p * p
-		[OUT, CUBIC]:
+		[OUT, QUINT]:
 			p -= 1
 			step = change * (p * p * p * p * p + 1)
-		[INOUT, CUBIC]:
+		[INOUT, QUINT]:
 			p /= 2
 			if p < 1:
 				step = (change / 2) * p * p * p * p * p
@@ -23,10 +25,10 @@ static func ease(from, to, progress, type=[null, LINEAR]):
 
 		[IN, QUART]:
 			step = change * p * p * p * p
-		[OUT, CUBIC]:
+		[OUT, QUART]:
 			p -= 1
 			step = -change * (p * p * p * p - 1)
-		[INOUT, CUBIC]:
+		[INOUT, QUART]:
 			p /= 2
 			if p < 1:
 				step = (change / 2) * p * p * p * p
@@ -50,7 +52,7 @@ static func ease(from, to, progress, type=[null, LINEAR]):
 		[IN, QUAD]:
 			step = change * p * p
 		[OUT, QUAD]:
-			step = change * p * (p - 2)
+			step = change * p * (2 - p)
 		[INOUT, QUAD]:
 			p /= 2
 			if p < 1:
