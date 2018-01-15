5f41f96bf86aca1618db2f2f7cbc27faf591c7bc
Adrien <adrien@arccoza.com>
2018-01-16 02:57:28 +0900

    Simplify fns, rename `res` to `step`

--- src/scripts/global.gd
+++ src/scripts/global.gd
@@ -4,69 +4,63 @@ enum {IN, OUT, INOUT, LINEAR, QUAD, CUBIC, QUART, QUINT}
 
 static func ease(from, to, cur, tot, type=[null, LINEAR]):
 	var per = cur / tot
-	var res = null
+	var step = null
 	var change = to - from
 	
 	match type:
 		[IN, QUINT]:
-			per = cur / tot
-			res = change * per * per * per * per * per
+			step = change * per * per * per * per * per
 		[OUT, CUBIC]:
-			per = cur / tot - 1
-			res = change * (per * per * per * per * per + 1)
+			per -= 1
+			step = change * (per * per * per * per * per + 1)
 		[INOUT, CUBIC]:
-			per = cur / tot / 2
+			per /= 2
 			if per < 1:
-				res = (change / 2) * per * per * per * per * per
+				step = (change / 2) * per * per * per * per * per
 			else:
 				per = per - 2
-				res = (change / 2) * (per * per * per * per + 2)
+				step = (change / 2) * (per * per * per * per + 2)
 
 		[IN, QUART]:
-			per = cur / tot
-			res = change * per * per * per * per
+			step = change * per * per * per * per
 		[OUT, CUBIC]:
-			per = cur / tot - 1
-			res = -change * (per * per * per * per - 1)
+			per -= 1
+			step = -change * (per * per * per * per - 1)
 		[INOUT, CUBIC]:
-			per = cur / tot / 2
+			per /= 2
 			if per < 1:
-				res = (change / 2) * per * per * per * per
+				step = (change / 2) * per * per * per * per
 			else:
 				per = per - 2
-				res = (-change / 2) * (per * per * per * per - 2)
+				step = (-change / 2) * (per * per * per * per - 2)
 
 		[IN, CUBIC]:
-			per = cur / tot
-			res = change * per * per * per
+			step = change * per * per * per
 		[OUT, CUBIC]:
-			per = cur / tot - 1
-			res = change * (per * per * per + 1)
+			per -= 1
+			step = change * (per * per * per + 1)
 		[INOUT, CUBIC]:
-			per = cur / tot / 2
+			per /= 2
 			if per < 1:
-				res = (change / 2) * per * per * per
+				step = (change / 2) * per * per * per
 			else:
-				per = per - 2
-				res = (change / 2) * (per * per * per + 2)
+				per -= 2
+				step = (change / 2) * (per * per * per + 2)
 		
 		[IN, QUAD]:
-			per = cur / tot
-			res = change * per * per
+			step = change * per * per
 		[OUT, QUAD]:
-			per = cur / tot
-			res = change * per * (per - 2)
+			step = change * per * (per - 2)
 		[INOUT, QUAD]:
-			per = cur / tot / 2
+			per /= 2
 			if per < 1:
-				res = (change / 2) * per * per
+				step = (change / 2) * per * per
 			else:
-				res = (-change / 2) * ((--per) * (per - 2) - 1)
+				step = (-change / 2) * ((--per) * (per - 2) - 1)
 		
 		[_, LINEAR]: continue
 		[]: continue
 		[..]:
-			per = cur / tot
-			res = change * per
+			step = change * per
 	
-	return from + res
+	return from + step
