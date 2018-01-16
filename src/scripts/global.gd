83fdd8428b35f9b7e82e689796de99f3a3f80725
Adrien <adrien@arccoza.com>
2018-01-16 10:00:46 +0900

    Refactor easing fns

--- src/scripts/global.gd
+++ src/scripts/global.gd
@@ -5,64 +5,40 @@ enum {IN, OUT, INOUT, LINEAR, QUAD, CUBIC, QUART, QUINT}
 static func ease(from, to, progress, type=[null, LINEAR]):
 	#if progress >= 1.0: return to
 	var p = progress  # 0.0 -> 1.0
-	var step = null
 	var change = to - from
-	print("p: ", p, " change: ", change)
+	#print("p: ", p, " change: ", change)
 	
 	match type:
-		[IN, QUINT]:
-			step = change * p * p * p * p * p
-		[OUT, QUINT]:
-			p -= 1
-			step = change * (p * p * p * p * p + 1)
-		[INOUT, QUINT]:
-			p /= 2
-			if p < 1:
-				step = (change / 2) * p * p * p * p * p
-			else:
-				p = p - 2
-				step = (change / 2) * (p * p * p * p + 2)
-
 		[IN, QUART]:
-			step = change * p * p * p * p
+			p = p * p * p * p
 		[OUT, QUART]:
 			p -= 1
-			step = -change * (p * p * p * p - 1)
+			p = 1 - p * p * p * p 
 		[INOUT, QUART]:
-			p /= 2
-			if p < 1:
-				step = (change / 2) * p * p * p * p
+			if p < 0.5:
+				p = 8 * p * p * p * p
 			else:
-				p = p - 2
-				step = (-change / 2) * (p * p * p * p - 2)
+				p -= 1
+				p = 1 - 8 * p * p * p * p
 
 		[IN, CUBIC]:
-			step = change * p * p * p
+			p = p * p * p
 		[OUT, CUBIC]:
 			p -= 1
-			step = change * (p * p * p + 1)
+			p = p * p * p + 1
 		[INOUT, CUBIC]:
-			p /= 2
-			if p < 1:
-				step = (change / 2) * p * p * p
-			else:
-				p -= 2
-				step = (change / 2) * (p * p * p + 2)
-		
+			p = 4 * p * p * p if p < 0.5 else (p - 1) * (2 * p - 2) * (2 * p - 2) + 1
+
 		[IN, QUAD]:
-			step = change * p * p
+			p = p * p
 		[OUT, QUAD]:
-			step = change * p * (2 - p)
+			p = p * (2 - p)
 		[INOUT, QUAD]:
-			p /= 2
-			if p < 1:
-				step = (change / 2) * p * p
-			else:
-				step = (-change / 2) * ((--p) * (p - 2) - 1)
+			p = 2 * p * p if p < 0.5 else (4 - 2 * p) * p - 1
 		
 		[_, LINEAR]: continue
 		[]: continue
 		[..]:
-			step = change * p
+			p = p
 	
-	return from + step
+	return from + p * change
