2aba85d4a5310bd2fa653d6b3decf97d25ba7eb6
Adrien <adrien@arccoza.com>
2018-01-25 16:38:39 +0900

    Add new Missile2D type

--- src/addons/character2d/main.gd
+++ src/addons/character2d/main.gd
@@ -3,6 +3,7 @@ extends EditorPlugin
 
 var CharacterMeter = preload("character-meter.gd")
 var Character2D = preload("character2d.gd")
+var Missile2D = preload("missile2d.gd")
 
 func _enter_tree():
 	# Initialization of the plugin goes here
@@ -10,6 +11,7 @@ func _enter_tree():
 #	var Character2D = preload("character2d.gd")
 	add_custom_type("CharacterMeter", "Resource", CharacterMeter, preload("character-meter.png"))
 	add_custom_type("Character2D", "KinematicBody2D", Character2D, preload("character2d.png"))
+	add_custom_type("Missile2D", "Node2D", Missile2D, preload("missile2d.png"))
 	print("Character2D plugin loaded.")
 
 func _exit_tree():
