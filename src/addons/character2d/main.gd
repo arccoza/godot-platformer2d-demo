1deb2c450b7be295aaa11c94412c454b63ced388
Adrien <adrien@arccoza.com>
2018-01-26 17:06:05 +0900

    Refactor character plugin, rename missile2d -> projectiles2d

--- src/addons/character2d/main.gd
+++ src/addons/character2d/main.gd
@@ -3,15 +3,15 @@ extends EditorPlugin
 
 var CharacterMeter = preload("character-meter.gd")
 var Character2D = preload("character2d.gd")
-var Missile2D = preload("missile2d.gd")
+var Projectiles2D = preload("projectiles2d.gd")
 
 func _enter_tree():
 	# Initialization of the plugin goes here
 #	var CharacterMeter = preload("character-meter.gd")
 #	var Character2D = preload("character2d.gd")
-	add_custom_type("CharacterMeter", "Resource", CharacterMeter, preload("character-meter.png"))
-	add_custom_type("Character2D", "KinematicBody2D", Character2D, preload("character2d.png"))
-	add_custom_type("Missile2D", "Node2D", Missile2D, preload("missile2d.png"))
+	add_custom_type("CharacterMeter", "Resource", CharacterMeter, preload("icons/character-meter.png"))
+	add_custom_type("Character2D", "KinematicBody2D", Character2D, preload("icons/character2d.png"))
+	add_custom_type("Projectiles2D", "Node2D", Projectiles2D, preload("icons/projectiles2d.png"))
 	print("Character2D plugin loaded.")
 
 func _exit_tree():
