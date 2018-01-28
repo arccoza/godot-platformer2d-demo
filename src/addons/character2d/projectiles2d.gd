6fcbce7344a67b89aeb079a43124ada51965e462
Adrien <adrien@arccoza.com>
2018-01-28 21:55:37 +0900

    Add todos and notes for improving the emitter and removing the flicker

--- src/addons/character2d/projectiles2d.gd
+++ src/addons/character2d/projectiles2d.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
-export(int) var count = 5
+export(int) var count = 10
 export(int) var cycles = -1
 export(float) var period = 1.0
-export(float, 0, 1) var delay = 0.5
-export(float, 0, 1) var lifetime = 1.0
-export var velocity = Vector2(100, 0)
+export(float, 0, 1) var delay = 1.0
+export(float) var lifetime = 5.0
+export var velocity = Vector2(300, 0)
 export var gravity = Vector2(0, 0)
 export(PackedScene) var projectile
 
@@ -33,10 +33,14 @@ func _ready():
 	elif get_child_count():
 		projectile = get_child(0)
 		remove_child(projectile)
+	print(options.rate)
 
 func _physics_process(delta):
 	if firing:
 		_emit_process(delta)
+#		add_child(_instance(options))
+#		for c in get_children():
+#			c.update(delta)
 
 func _emit_process(delta):
 	if (options.cycles > 0 and cycle_count >= options.cycles) or options.cycles == 0:
@@ -52,7 +56,7 @@ func _emit_process(delta):
 		emit_time = 0
 		cycle_count += 1 if options.cycles > 0 else 0
 		emit_count = 0
-		print("cycle")
+#		print("cycle")
 		return
 	
 	cycle_time += delta
@@ -62,20 +66,25 @@ func _emit_process(delta):
 	
 	emit_time += delta
 	
-	if options.rate == 0 or options.rate < delta:
+	if options.rate == 0:
 		for i in range(0, options.count - emit_count):
 			emit_count += 1
-			print("tick")
+#			print("tick")
 			add_child(_instance(options))
 		emit_time = 0
 	elif emit_time >= options.rate:
-		print("tick", " - ", emit_time)
+#		print("tick", " - ", emit_time)
 		add_child(_instance(options))
 		emit_time -= options.rate
 		emit_count += 1
 
 func _instance(o):
-	return Projectile2D.new(o.lifetime, o.velocity, o.gravity, projectile.duplicate())
+	# TODO: Recycle projectile nodes,
+	# rather than constantly creating and destroying them.
+	# Also manage the creation, reuse, and destruction better.
+	var p = projectile.duplicate()
+#	print(p)
+	return Projectile2D.new(o.lifetime, o.velocity, o.gravity, p)
 
 func fire(opts=null):
 	options = merge(defaults.duplicate(), opts) if opts else defaults
