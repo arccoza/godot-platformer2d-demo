1706ff0d3c23c35d4e8f4f481f4a1205750ffcb6
Adrien <adrien@arccoza.com>
2018-02-16 08:48:33 +0900

    Update plugin with NPC node and bump version

--- src/addons/character2d/main.gd
+++ src/addons/character2d/main.gd
@@ -3,6 +3,7 @@ extends EditorPlugin
 
 var CharacterMeter = preload("character-meter.gd")
 var Character2D = preload("character2d.gd")
+var NonPlayerCharacter2D = preload("non-player-character2d.gd")
 var Projectiles2D = preload("projectiles2d.gd")
 var AreaProjectile2D = preload("area-projectile2d.gd")
 
@@ -10,8 +11,9 @@ func _enter_tree():
 	# Initialization of the plugin goes here
 #	var CharacterMeter = preload("character-meter.gd")
 #	var Character2D = preload("character2d.gd")
-	add_custom_type("CharacterMeter", "Resource", CharacterMeter, preload("icons/character-meter.png"))
+#	add_custom_type("CharacterMeter", "Resource", CharacterMeter, preload("icons/character-meter.png"))
 	add_custom_type("Character2D", "KinematicBody2D", Character2D, preload("icons/character2d.png"))
+	add_custom_type("NonPlayerCharacter2D", "KinematicBody2D", NonPlayerCharacter2D, preload("icons/character2d.png"))
 	add_custom_type("Projectiles2D", "Node2D", Projectiles2D, preload("icons/projectiles2d.png"))
 	add_custom_type("AreaProjectile2D", "Area2D", AreaProjectile2D, preload("icons/projectiles2d.png"))
 	print("Character2D plugin loaded.")
