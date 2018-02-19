91898e5f453815c9986af590a745a1f8ba74d349
Adrien <adrien@arccoza.com>
2018-02-19 17:54:31 +0900

    Add `VisionCone2D` node type to plugin

--- src/addons/character2d/main.gd
+++ src/addons/character2d/main.gd
@@ -4,6 +4,7 @@ extends EditorPlugin
 var CharacterMeter = preload("character-meter.gd")
 var Character2D = preload("character2d.gd")
 var NonPlayerCharacter2D = preload("non-player-character2d.gd")
+var VisionCone2D = preload("vision-cone2d.gd")
 var Projectiles2D = preload("projectiles2d.gd")
 var AreaProjectile2D = preload("area-projectile2d.gd")
 
@@ -14,6 +15,7 @@ func _enter_tree():
 #	add_custom_type("CharacterMeter", "Resource", CharacterMeter, preload("icons/character-meter.png"))
 	add_custom_type("Character2D", "KinematicBody2D", Character2D, preload("icons/character2d.png"))
 	add_custom_type("NonPlayerCharacter2D", "KinematicBody2D", NonPlayerCharacter2D, preload("icons/character2d.png"))
+	add_custom_type("VisionCone2D", "Node2D", VisionCone2D, preload("icons/vision-cone2d.svg"))
 	add_custom_type("Projectiles2D", "Node2D", Projectiles2D, preload("icons/projectiles2d.png"))
 	add_custom_type("AreaProjectile2D", "Area2D", AreaProjectile2D, preload("icons/projectiles2d.png"))
 	print("Character2D plugin loaded.")
