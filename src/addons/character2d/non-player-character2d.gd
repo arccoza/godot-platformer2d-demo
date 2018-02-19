e573f8345204009d280dad550182f725cf0d8898
Adrien <adrien@arccoza.com>
2018-02-19 17:55:18 +0900

    Add `VisionCone2D` to NPC node type

--- src/addons/character2d/non-player-character2d.gd
+++ src/addons/character2d/non-player-character2d.gd
@@ -1,28 +1,36 @@
 extends 'character2d.gd'
+var VisionCone2D = preload("vision-cone2d.gd")
 
 export(int) var wander_period = 3
 export(float, 0, 1, 0.05) var wander_pause = 0.5
 var wander_time = 0
 export(int) var alert_delay = 2
 var alert_time = 0
-
 var alert = false
-var vision = null
+var vision_cone = null
 
 
 func _ready():
-	vision = $vision
-	$step_limit.add_exception(vision)
-	vision.connect("body_entered", self, "_on_vision_detect", [true])
-	vision.connect("body_exited", self, "_on_vision_detect", [false])
+	for c in get_children():
+		if c is VisionCone2D:
+			vision_cone = c
+	
+	_init_vision()
 	upd_ai_times(0)
+
+func _init_vision():
+	if not vision_cone:
+		return
+	$step_limit.add_exception(vision_cone)
+	vision_cone.connect("found", self, "_on_vision_detect", [true])
+	vision_cone.connect("lost", self, "_on_vision_detect", [false])
 
 func _physics_process(delta):
 	upd_ai_times(delta)
 	if direction.x:
-		vision.scale = Vector2(direction.x, 1)
+		vision_cone.scale = Vector2(direction.x, 1)
 
-func _on_vision_detect(ev, entered):
+func _on_vision_detect(ev, is_area, entered):
 	if ev.is_in_group("player"):
 		alert = entered
 #		print(ev.name, alert)
