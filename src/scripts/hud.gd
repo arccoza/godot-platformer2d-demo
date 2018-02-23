63ef21f50cf4c3e3058c8badaa1405d28436db84
Adrien <adrien@arccoza.com>
2018-02-23 17:39:15 +0900

    Add hud scene and resource bar images

---
+++ src/scripts/hud.gd
@@ -0,0 +1,23 @@
+extends MarginContainer
+
+export(NodePath) var player_node
+var player = null
+var res = {}
+
+
+func _ready():
+	if player_node:
+		player = get_node(player_node)
+		res["health"] = player.get("health")
+		res["energy"] = player.get("energy")
+		res["points"] = player.get("points")
+		
+		for k in res:
+			res[k].connect("changed", self, "resource_changed", [k])
+
+func resource_changed(value, name):
+	var n = find_node(name + "_bar")
+	prints(n)
+	
+	if n:
+		n.value = value
