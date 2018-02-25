8712c1d3d586e1279c3c30cb19248c7a6dfb67fe
Adrien <adrien@arccoza.com>
2018-02-25 23:36:27 +0900

    Update UI player_name field with name from character

--- src/scripts/hud.gd
+++ src/scripts/hud.gd
@@ -12,6 +12,8 @@ func _ready():
 		res["energy"] = player.get("energy")
 		res["points"] = player.get("points")
 		
+		player_name_changed(player.player_name)
+		
 		for k in res:
 			if res[k]:
 				res[k].connect("changed", self, "resource_changed", [k])
@@ -19,6 +21,10 @@ func _ready():
 
 # TODO: add code to handle max and min bounds if different between hud and character.
 # May have to add a new signal to Quant for when bounds change (eg. max increased).
+
+func player_name_changed(name):
+	var n = find_node("player_name")
+	n.text = name
 
 func resource_changed(value, name):
 	var n = find_node(name + "_bar")
