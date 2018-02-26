7de1934b1d737abc4485c8306c82eba102785331
Adrien <adrien@arccoza.com>
2018-02-27 08:46:50 +0900

    Add points `Quant` to player

--- src/addons/character2d/player-character2d.gd
+++ src/addons/character2d/player-character2d.gd
@@ -2,6 +2,11 @@ extends "character2d.gd"
 
 export var player_name = "Bob"
 
+export var points_value = 100
+var points_min = 0
+export var points_max = 100
+var points = Quant.new(points_value, points_min, points_max)
+
 
 func _ready():
 	pass
