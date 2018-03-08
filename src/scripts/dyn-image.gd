32cb7b7d085ceda338c4cba77a0766eb5775d206
Adrien <adrien@arccoza.com>
2018-03-08 23:03:12 +0900

    Update scenes

--- src/scripts/dyn-image.gd
+++ src/scripts/dyn-image.gd
@@ -1,5 +1,5 @@
 tool
-extends Sprite
+extends Light2D
 
 
 
@@ -18,19 +18,33 @@ func _ready():
 		for y in range(0, size.y):
 			uv.y = float(y) / (size.y - 1)
 #			prints(x, y, uv)
-			img.set_pixel(x, y, Color(1,0,0,uv.x))
+			img.set_pixel(x, y, Color(1,1,1,uv.x))
 	img.unlock()
-	prints(uv)
+#	prints(uv)
 
-	prints(img.data["data"].size())
+#	prints(img.data["data"].size())
 	
 	tex.create_from_image(img, Texture.FLAG_FILTER)
 	self.texture = tex
 	
-	for k in img.data:
-		prints(k)
-	
-	
+#	for k in img.data:
+#		prints(k)
+	prints("data: ", tex.get_data())
 	
 	tex.resource_name = "The created texture!"
 	print(self.texture.resource_name)
+	
+	var tmp = LightTexture.new()
+	print(tmp)
+
+
+class LightTexture extends ImageTexture:
+	enum {GRADIENT_LINEAR, GRADIENT_RADIAL}
+	export(int, "Linear", "Radial") var gradient_type
+	export(bool) var reflected = false
+	export(float, 0, 1) var soften_edges = 0
+	export var size = Vector2(64, 64)
+	var image = Image.new()
+	
+	func _init():
+		image.create(size.x, size.y, true, Image.FORMAT_RGBA8)
